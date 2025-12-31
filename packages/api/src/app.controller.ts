9c41a0cea7a26ea45ee8ea5b0ebaef21d79a451a
kaliktg <86680782+kaliktg@users.noreply.github.com>
2025-12-31 11:44:15 +0300

    fix(api): add @Public decorator to root endpoint for health check

--- packages/api/src/app.controller.ts
+++ packages/api/src/app.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get } from "@nestjs/common";
 import { AppService } from "#api/app.service";
+import { Public } from "#api/common/decorators/public.decorator";
 
 @Controller()
 export class AppController {
 	constructor(private readonly appService: AppService) {}
 
+	@Public()
 	@Get()
 	getHello(): string {
 		return this.appService.getHello();
